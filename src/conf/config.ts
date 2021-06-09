3dca96db6af73b02d069c7771b35fe783e4d9bf4
Alexandre Annic <alexandre.annic1@gmail.com>
2021-06-09 22:22:13 +0200

    Try to setup env variables

--- src/conf/config.ts
+++ src/conf/config.ts
@@ -1,7 +1,9 @@
+console.log(process.env)
+
 export class Config {
   static readonly isDev = process.env.NODE_ENV === 'development'
 
-  static readonly baseUrl = 'http://localhost:9000'
+  static readonly baseUrl = process.env.API_BASE_URL ?? 'http://localhost:9000'
 
   static readonly sentryDsn = null
 
