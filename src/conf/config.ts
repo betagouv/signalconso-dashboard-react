64b239b583d3cc5572e60d2e31ff1cd0684dc78c
Alexandre Annic <alexandre.annic1@gmail.com>
2021-06-10 08:27:19 +0200

    Fix env

--- src/conf/config.ts
+++ src/conf/config.ts
@@ -1,9 +1,8 @@
-console.log(process.env)
-
+// Env variables must start with 'REACT_APP_' to be considered by CreateReactApp
 export class Config {
   static readonly isDev = process.env.NODE_ENV === 'development'
 
-  static readonly baseUrl = process.env.API_BASE_URL ?? 'http://localhost:9000'
+  static readonly baseUrl = process.env.REACT_APP_API_BASE_URL ?? 'http://localhost:9000'
 
   static readonly sentryDsn = null
 
