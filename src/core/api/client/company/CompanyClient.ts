c54b39e5db7e1a60c9809b49ceed81a5535b41e0
Alexandre Annic <alexandre.annic1@gmail.com>
2021-08-05 09:47:55 +0300

    Remove mock

--- src/core/api/client/company/CompanyClient.ts
+++ src/core/api/client/company/CompanyClient.ts
@@ -52,31 +52,12 @@ export class CompanyClient {
   }
 
   readonly getCompaniesAccessibleByPro = (): Promise<CompanyWithAccessLevel[]> => {
-    // return this.client.get<CompanyWithAccessLevel[]>(`/accesses/connected-user`)
-    return Promise.resolve([{
-      'id': 'db146dca-e087-4f82-af8f-e4c9e6a8b60f',
-      'siret': '81352117600038',
-      'creationDate': '2021-04-08T07:38:29.371639Z',
-      'name': 'ALEXANDRE ANNIC',
-      'address': {'number': '31', 'street': 'RUE EMILE BERNARD', 'addressSupplement': 'More', 'postalCode': '29930', 'city': 'PONT-AVEN'},
-      'activityCode': '62.02A',
-      'level': 'admin' as any,
-    },
-    //   {
-    //   'id': 'b1a01fc4-34f8-453e-87e7-b89bf76f5267',
-    //   'siret': '81352117600020',
-    //   'creationDate': '2021-02-19T18:26:23.920244Z',
-    //   'name': 'ALEXANDRE ANNIC',
-    //   'address': {'number': '5', 'street': 'RUE JEAN MACE', 'addressSupplement': '', 'postalCode': '92150', 'city': 'SURESNES'},
-    //   'activityCode': '62.02A',
-    //   'level': 'admin' as any,
-    // }
-    ]).then(res => res.map(_ => ({..._, creationDate: new Date(_.creationDate)})))
+    return this.client.get<CompanyWithAccessLevel[]>(`/accesses/connected-user`)
+      .then(res => res.map(_ => ({..._, creationDate: new Date(_.creationDate)})))
   }
 
   readonly getCompaniesVisibleByPro = (): Promise<VisibleCompany[]> => {
-    // return this.client.get<ViewableCompany[]>(`/companies/connected-user`)
-    return Promise.resolve([{'siret': '81352117600038', 'postalCode': '29930', 'closed': false}, {'siret': '81352117600020', 'postalCode': '92150', 'closed': true}])
+    return this.client.get<VisibleCompany[]>(`/companies/connected-user`)
   }
 
   readonly fetchToActivate = () => {
