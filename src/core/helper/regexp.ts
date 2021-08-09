50fd1faad18511e967ed7333fdd244b8f2f7de2b
Said SEDOUD <s.sedoud.betagouv@gmail.com>
2021-08-09 12:19:56 +0200

    fix dgccrf email regexp

--- src/core/helper/regexp.ts
+++ src/core/helper/regexp.ts
@@ -1,6 +1,6 @@
 export const regexpPattern = {
   email: '^[^@]+@[^\\.]+\\..+',
-  emailDGCCRF: '^[^@]+@[^\\.]+\\.gouv\\.fr',
+  emailDGCCRF: '^[^@]+@[a-zA-Z0-9_\\-.]*\\.gouv\\.fr',
   yyyyMMdd: '\\d{4}-(0\\d|1[0-2])-([0-2]\\d|3[0-1])',
 }
 
