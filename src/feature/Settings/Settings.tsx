f738585df31cef5b96ea7ac594ffddd58014e9bc
Alexandre Annic <alexandre.annic1@gmail.com>
2021-09-04 10:59:23 +0200

    Display notification config only for pro

--- src/feature/Settings/Settings.tsx
+++ src/feature/Settings/Settings.tsx
@@ -7,9 +7,11 @@ import {NavLink} from 'react-router-dom'
 import {siteMap} from '../../core/siteMap'
 import {ScButton} from '../../shared/Button/Button'
 import {EditPasswordDialog} from './EditPasswordDialog'
+import {useLogin} from '../../core/context/LoginContext'
 
 export const Settings = () => {
   const {m} = useI18n()
+  const {connectedUser} = useLogin()
 
   return (
     <Page size="small">
@@ -22,13 +24,15 @@ export const Settings = () => {
             </ScButton>
           </EditPasswordDialog>
         </SettingRow>
-        <SettingRow icon="notifications" title={m.notifications} description={m.notificationSettings}>
-          <NavLink to={siteMap.companiesPro}>
-            <ScButton icon="edit" color="primary">
-              {m.edit}
-            </ScButton>
-          </NavLink>
-        </SettingRow>
+        {connectedUser.isPro && (
+          <SettingRow icon="notifications" title={m.notifications} description={m.notificationSettings}>
+            <NavLink to={siteMap.companiesPro}>
+              <ScButton icon="edit" color="primary">
+                {m.edit}
+              </ScButton>
+            </NavLink>
+          </SettingRow>
+        )}
       </Panel>
     </Page>
   )
