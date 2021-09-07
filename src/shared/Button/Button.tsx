7d1c3ed1e46948ed6eb45e46f2e4884f5e90f641
Alexandre Annic <alexandre.annic1@gmail.com>
2021-09-07 17:50:57 +0200

    ScButton: Add missing forwardRef

--- src/shared/Button/Button.tsx
+++ src/shared/Button/Button.tsx
@@ -1,6 +1,7 @@
 import {Btn} from 'mui-extension/lib'
 import {BtnProps} from 'mui-extension/lib/Btn/Btn'
+import {forwardRef} from 'react'
 
-export const ScButton = (props: BtnProps) => {
-  return <Btn {...props} />
-}
+export const ScButton = forwardRef((props: BtnProps, ref: any) => {
+  return <Btn {...props} ref={ref} />
+})
