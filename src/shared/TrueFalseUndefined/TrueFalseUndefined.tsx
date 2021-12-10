3c68688ab6b4e2cfe93ab3f0efd53df95e462bf9
Alexandre Annic <alexandre.annic1@gmail.com>
2021-12-10 12:30:38 +0100

    Users: filters active

--- src/shared/TrueFalseUndefined/TrueFalseUndefined.tsx
+++ src/shared/TrueFalseUndefined/TrueFalseUndefined.tsx
@@ -8,6 +8,11 @@ interface Props {
   gutter?: boolean,
   value?: boolean
   onChange: (_?: boolean) => void
+  label?: {
+    true?: string
+    false?: string
+    undefined?: string
+  }
 }
 
 const buttonStyle: SxProps<Theme> = {
@@ -16,12 +21,14 @@ const buttonStyle: SxProps<Theme> = {
   // paddingBottom: .5,
   paddingRight: 1.5,
   paddingLeft: 1.5,
+  whiteSpace: 'nowrap'
 }
 
 export const TrueFalseUndefined = forwardRef(({
   gutter,
   value,
   onChange,
+  label,
   ...props
 }: Props, ref: any) => {
   const {m} = useI18n()
@@ -52,9 +59,9 @@ export const TrueFalseUndefined = forwardRef(({
         }
       }}
     >
-      <ToggleButton sx={buttonStyle} value="true">{m.yes}</ToggleButton>
-      <ToggleButton sx={buttonStyle} value="false">{m.no}</ToggleButton>
-      <ToggleButton sx={buttonStyle} value="">{m.indifferent}</ToggleButton>
+      <ToggleButton sx={buttonStyle} value="true">{label?.true ?? m.yes}</ToggleButton>
+      <ToggleButton sx={buttonStyle} value="false">{label?.false ?? m.no}</ToggleButton>
+      <ToggleButton sx={buttonStyle} value="">{label?.undefined ?? m.indifferent}</ToggleButton>
     </ToggleButtonGroup>
   )
 })
