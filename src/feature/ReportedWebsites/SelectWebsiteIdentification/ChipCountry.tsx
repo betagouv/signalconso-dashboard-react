a52ae63311618ec865e28403ae5e8d69ce4d3fe8
ssedoudbgouv <s.sedoud.betagouv@gmail.com>
2022-06-15 16:14:21 +0200

    TRELLO-1015: remove unused

--- src/feature/ReportedWebsites/SelectWebsiteIdentification/ChipCountry.tsx
+++ src/feature/ReportedWebsites/SelectWebsiteIdentification/ChipCountry.tsx
@@ -3,25 +3,13 @@ import {Txt} from 'mui-extension/lib/Txt/Txt'
 import React from 'react'
 import {useI18n} from '../../../core/i18n'
 import {Country} from '@signal-conso/signalconso-api-sdk-js'
-import {makeSx} from 'mui-extension'
 import {styleUtils} from '../../../core/theme'
 import {ScChip} from '../../../shared/Chip/ScChip'
+import {countryToFlag} from "../../../core/helper/utils";
 
 interface Props extends BoxProps {
   country: Country
 }
-
-const countryToFlag = (isoCode: string) => {
-  return typeof String.fromCodePoint !== 'undefined'
-    ? isoCode.toUpperCase().replace(/./g, char => String.fromCodePoint(char.charCodeAt(0) + 127397))
-    : isoCode
-}
-
-const sx = makeSx({
-  tdName_desc: t => ({
-
-  }),
-})
 
 export const ChipCountry = ({country, ...props}: Props) => {
   const {m} = useI18n()
