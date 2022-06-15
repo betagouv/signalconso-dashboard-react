84bd1e7288168e8637af6e1cf644bc9640ea9c41
eletallbetagouv <107104509+eletallbetagouv@users.noreply.github.com>
2022-06-15 11:03:33 +0200

    Revert "trello-1256: Fix TrueFalseUndefined"
    This reverts commit ea6bbe0a3cc7241d3b5958d1c6503abc708abebe.

--- src/feature/Reports/ReportsFilters.tsx
+++ src/feature/Reports/ReportsFilters.tsx
@@ -46,7 +46,7 @@ const rationalizeFilters = (f: ReportSearch): ReportSearch => ({
 
 export const toReportTagValues = <T extends Pick<ReportSearch, 'withTags' | 'withoutTags'>>(
   filters: T,
-): T & { tags: SelectTagsMenuValues } => {
+): T & {tags: SelectTagsMenuValues} => {
   const tags: SelectTagsMenuValues = {}
   filters.withTags?.forEach(tag => {
     tags[tag] = 'included'
@@ -145,7 +145,6 @@ const _ReportsFilters = ({filters, updateFilters, children}: _ReportsFiltersProp
                   control={control}
                   render={({field}) => (
                     <TrueFalseUndefined
-                      emitEmptyString
                       label={{
                         true: <TrueLabel />,
                       }}
@@ -222,7 +221,6 @@ const _ReportsFilters = ({filters, updateFilters, children}: _ReportsFiltersProp
                   control={control}
                   render={({field}) => (
                     <TrueFalseUndefined
-                      emitEmptyString
                       {...field}
                       label={{
                         true: <TrueLabel />,
@@ -244,7 +242,6 @@ const _ReportsFilters = ({filters, updateFilters, children}: _ReportsFiltersProp
                   control={control}
                   render={({field}) => (
                     <TrueFalseUndefined
-                      emitEmptyString
                       {...field}
                       label={{
                         true: <TrueLabel />,
@@ -266,7 +263,6 @@ const _ReportsFilters = ({filters, updateFilters, children}: _ReportsFiltersProp
                   control={control}
                   render={({field}) => (
                     <TrueFalseUndefined
-                      emitEmptyString
                       {...field}
                       label={{
                         true: <TrueLabel />,
@@ -296,7 +292,6 @@ const _ReportsFilters = ({filters, updateFilters, children}: _ReportsFiltersProp
                   control={control}
                   render={({field: {value, onChange, ...otherField}}) => (
                     <TrueFalseUndefined
-                      emitEmptyString
                       {...otherField}
                       value={!value}
                       onChange={_ => onChange(_ === undefined ? undefined : !_)}
@@ -310,9 +305,7 @@ const _ReportsFilters = ({filters, updateFilters, children}: _ReportsFiltersProp
                   name="hasAttachment"
                   defaultValue={filters.hasAttachment}
                   control={control}
-                  render={({field}) => <TrueFalseUndefined
-                    emitEmptyString
-                    {...field} sx={{mt: 1}} />}
+                  render={({field}) => <TrueFalseUndefined {...field} sx={{mt: 1}} />}
                 />
               </DialogInputRow>
             </DialogContent>
