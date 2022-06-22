5032b99bbb20768f9e962260871c0e640c93bde0
eletallbetagouv <107104509+eletallbetagouv@users.noreply.github.com>
2022-06-22 21:22:04 +0200

    fixes and delete some useless stuff

--- src/alexlibs/ts-utils/index.ts
+++ src/alexlibs/ts-utils/index.ts
@@ -1,5 +1,1 @@
 export * from './common'
-// Prevent global export for compatibility purpose:
-// Importing ./node in a front application will make it crash.
-// export * from './node';
-// export * from './browser';
