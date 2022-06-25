233b0a305e44e2fdb7ee7d634f5efeb0ac1436ff
eletallbetagouv <107104509+eletallbetagouv@users.noreply.github.com>
2022-06-25 10:30:01 +0200

    delete unused

--- src/alexlibs/mui-extension/Autocomplete/index.ts
+++
@@ -1,4 +0,0 @@
-export {Autocomplete} from './Autocomplete'
-export {AutocompleteItem} from './AutocompleteItem'
-export type {AutocompleteProps} from './Autocomplete'
-export type {AutocompleteItemProps} from './AutocompleteItem'
