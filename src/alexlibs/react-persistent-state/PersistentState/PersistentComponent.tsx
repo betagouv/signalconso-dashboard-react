1eb691c7956a3a75408f15c20dc3914c6bd889f5
eletallbetagouv <107104509+eletallbetagouv@users.noreply.github.com>
2022-08-17 11:18:56 +0200

    remove lodash.throttle, lodash.uniqby

--- src/alexlibs/react-persistent-state/PersistentState/PersistentComponent.tsx
+++ src/alexlibs/react-persistent-state/PersistentState/PersistentComponent.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import {LocalStorageEntity} from '../utils/localStorageApi'
 import {generateId} from '../utils/hash'
-import throttle from 'lodash.throttle'
+import { throttle } from 'core/lodashNamedExport'
 
 export abstract class PersistentComponent<P = {}, S = {}, SS = any> extends React.Component<P, S, SS> {
   private localStorage: LocalStorageEntity<S>
