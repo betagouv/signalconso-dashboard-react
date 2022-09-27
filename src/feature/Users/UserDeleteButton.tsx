f0e0403dd94af0aef8d22e7c0295333bf84c6a00
eletallbetagouv <107104509+eletallbetagouv@users.noreply.github.com>
2022-09-27 20:34:30 +0200

    refine texts etc.

--- src/feature/Users/UserDeleteButton.tsx
+++ src/feature/Users/UserDeleteButton.tsx
@@ -1,6 +1,6 @@
-import {Icon, Tooltip} from '@mui/material'
+import {Tooltip} from '@mui/material'
 import {useEffect} from 'react'
-import {Btn, IconBtn} from '../../alexlibs/mui-extension'
+import {Alert, Btn, Txt} from '../../alexlibs/mui-extension'
 import {useUsersContext} from '../../core/context/UsersContext'
 import {useI18n} from '../../core/i18n'
 
@@ -24,18 +24,29 @@ export const UserDeleteButton = ({userId, compact}: Props) => {
     }
   }, [err, toastError])
 
+  const dialogContent = (
+    <>
+      <Txt bold>{m.delete_user_desc_main}</Txt>
+      <Alert type="warning" sx={{mb: 2, mt: 2}} dense>
+        <Txt bold>{m.operation_irreversible}</Txt>
+      </Alert>
+      {m.delete_user_desc_details}
+    </>
+  )
+
   return (
     <ScDialog
-      title={m.removeAsk}
-      content={'attention delete irreversible bla bla'}
+      title={m.delete_user_ask}
+      content={dialogContent}
       onConfirm={(event, close) => {
         _softDelete
           .fetch({}, userId)
           .then(_ => close())
           .then(_ => toastSuccess(m.userValidationDone))
       }}
+      confirmLabel={m.delete_user}
     >
-      <Tooltip title={'delete user'}>
+      <Tooltip title={m.delete_user}>
         <Btn loading={_softDelete.loading} sx={{color: t => t.palette.error.main}} icon="delete">
           {compact ? null : m.delete}
         </Btn>
