4b9e3806742905f178a69d45a1aa7c46a93b225b
Charles Dufour <charles.dufour@beta.gouv.fr>
2023-10-20 09:31:39 +0200

    [TRELLO-2037] Rework users screen (#433)

--- src/feature/Users/UsersListPending.tsx
+++ src/feature/Users/UsersListPending.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import {Panel} from '../../shared/Panel'
 import {Datatable} from '../../shared/Datatable/Datatable'
 import {useUsersContext} from '../../core/context/UsersContext'
@@ -8,23 +8,17 @@ import {Txt} from '../../alexlibs/mui-extension'
 import {useToast} from '../../core/toast'
 import {ScDialog} from '../../shared/ScDialog'
 import {IconBtn} from '../../alexlibs/mui-extension'
-import {Icon, Tooltip} from '@mui/material'
+import {Box, Icon, ToggleButtonGroup, Tooltip} from '@mui/material'
 import {useLogin} from '../../core/context/LoginContext'
 import {sxUtils} from '../../core/theme'
 import {ScOption} from 'core/helper/ScOption'
-import {RoleAdminOrDggcrfOrDgal} from '../../core/client/user/User'
+import {RoleAdminOrAgent, roleAgents, RoleAgents} from '../../core/client/user/User'
+import {SelectRoleAgent} from '../../shared/SelectRoleAgent'
 
-export const DgccrfUsersListPending = () => <UsersListPending role="DGCCRF" />
-export const DgalUsersListPending = () => <UsersListPending role="DGAL" />
-
-interface Props {
-  role: 'DGCCRF' | 'DGAL'
-}
-
-const UsersListPending = ({role}: Props) => {
+export const UsersListPending = () => {
   const usersContext = useUsersContext()
-  const _users = role === 'DGCCRF' ? usersContext.dgccrfPending : usersContext.dgalPending
-  const _invite = role === 'DGCCRF' ? usersContext.inviteDgccrf : usersContext.inviteDgal
+  const _users = usersContext.agentPending
+  // const _invite = role === 'DGCCRF' ? usersContext.inviteDgccrf : usersContext.inviteDgal
   const {m, formatDate} = useI18n()
   const {connectedUser} = useLogin()
   const {toastError, toastSuccess} = useToast()
@@ -33,9 +27,15 @@ const UsersListPending = ({role}: Props) => {
     navigator.clipboard.writeText(activationLink).then(_ => toastSuccess(m.addressCopied))
   }
 
+  const [selectedRole, setSelectedRole] = useState<RoleAgents[]>(roleAgents.map(_ => _))
+
   useEffect(() => {
-    _users.fetch()
-  }, [])
+    if (selectedRole.length === 1) {
+      _users.fetch({}, selectedRole[0])
+    } else {
+      _users.fetch()
+    }
+  }, [selectedRole])
 
   useEffect(() => {
     ScOption.from(_users.error).map(toastError)
@@ -45,6 +45,11 @@ const UsersListPending = ({role}: Props) => {
     <Panel>
       <Datatable
         id="userslistpending"
+        header={
+          <Box sx={{display: 'flex', flexDirection: 'row-reverse', width: '100%'}}>
+            <SelectRoleAgent value={selectedRole} onChange={_ => setSelectedRole(_)} />
+          </Box>
+        }
         loading={_users.loading}
         total={_users.entity?.length}
         data={_users.entity}
@@ -70,11 +75,12 @@ const UsersListPending = ({role}: Props) => {
                     .map(email => (
                       <ScDialog
                         title={m.resendCompanyAccessToken(_.email)}
-                        onConfirm={(event, close) =>
-                          _invite
-                            .fetch({}, email)
-                            .then(_ => close())
-                            .then(_ => toastSuccess(m.userInvitationSent))
+                        onConfirm={
+                          (event, close) => null
+                          // _invite
+                          //   .fetch({}, email)
+                          //   .then(_ => close())
+                          //   .then(_ => toastSuccess(m.userInvitationSent))
                         }
                         maxWidth="xs"
                       >
