8d6b9ba08859e16eee2f6f9bc45712c619dcca27
Charles Dufour <charles.dufour@beta.gouv.fr>
2023-11-03 18:06:11 +0100

    [TRELLO-2048] Implement GS1 (find product/company by barcode)

--- src/core/client/gs1/GS1Product.ts
+++ src/core/client/gs1/GS1Product.ts
@@ -1,9 +1,19 @@
-import {Id} from '../../model'
+import {Address, Id} from '../../model'
 
 export interface GS1Product {
   id: Id
   gtin: string
+  productDescription?: string
+  brandName?: string
+  subBrandName?: string
+  netContent?: NetContent[]
+  globalLocationNumber?: string
+  companyName?: string
+  postalAddress?: Address
   siren?: string
-  description?: string
-  creationDate: Date
+}
+
+interface NetContent {
+  unitCode?: string
+  quantity?: string
 }
--- src/feature/Report/ReportGS1Product.tsx
+++ src/feature/Report/ReportGS1Product.tsx
@@ -4,9 +4,24 @@ import {Id} from '../../core/model'
 import {useApiContext} from '../../core/context/ApiContext'
 import {useQuery} from '@tanstack/react-query'
 import {WithInlineIcon} from '../../shared/WithInlineIcon'
+import {Chip, Grid, Paper} from '@mui/material'
 
 interface ReportGS1ProductProps {
   gs1ProductId: Id
+}
+
+interface RowProps {
+  label: string
+  value: string
+}
+
+const Row = ({label, value}: RowProps) => {
+  return (
+    <div className="flex w-1/2">
+      <span className="min-w-[30%]">{label} :</span>
+      <span> {value}</span>
+    </div>
+  )
 }
 
 export const ReportGS1Product = ({gs1ProductId}: ReportGS1ProductProps) => {
@@ -19,9 +34,48 @@ export const ReportGS1Product = ({gs1ProductId}: ReportGS1ProductProps) => {
   return (
     <Panel>
       <PanelHead>
-        <WithInlineIcon icon="shopping_cart">Fiche produit</WithInlineIcon>
+        <div className="flex justify-between">
+          <WithInlineIcon icon="shopping_cart">Fiche produit</WithInlineIcon>
+          <Chip label={`GTIN: ${data?.gtin}`} />
+        </div>
       </PanelHead>
-      <PanelBody>{data?.description}</PanelBody>
+      <PanelBody>
+        <Row label="Descprtion" value={data?.productDescription ?? 'N/A'} />
+        <Row label="Marque" value={data?.brandName ?? 'N/A'} />
+        <Row label="Sous-marque" value={data?.subBrandName ?? 'N/A'} />
+        <Grid sx={{mt: 2}} container spacing={2} alignItems="stretch">
+          <Grid item xs={12} sm={6}>
+            <Panel>
+              <PanelHead>
+                <WithInlineIcon icon="scale">Contenu</WithInlineIcon>
+              </PanelHead>
+              <PanelBody>
+                <ul>
+                  {data?.netContent?.map(netContent => (
+                    <li>
+                      {netContent.quantity} {netContent.unitCode}
+                    </li>
+                  ))}
+                </ul>
+              </PanelBody>
+            </Panel>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Panel>
+              <PanelHead>
+                <WithInlineIcon icon="scale">Entreprise</WithInlineIcon>
+              </PanelHead>
+              <PanelBody>
+                <div>Nom: {data?.companyName}</div>
+                <div>Ville: {data?.postalAddress?.city}</div>
+                <div>Code postal: {data?.postalAddress?.postalCode}</div>
+                <div>globalLocationNumber: {data?.globalLocationNumber}</div>
+                <div>Siren: {data?.siren}</div>
+              </PanelBody>
+            </Panel>
+          </Grid>
+        </Grid>
+      </PanelBody>
     </Panel>
   )
 }
