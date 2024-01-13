b03b3ad2aa044cf0eb7b992a1f115e47b01ab412
ssedoudbgouv <s.sedoud.betagouv@gmail.com>
2024-01-13 14:46:18 +0100

    TRELLO-2010: fix ui

--- src/feature/ReportedWebsites/WebsitesInvestigation.tsx
+++ src/feature/ReportedWebsites/WebsitesInvestigation.tsx
@@ -91,7 +91,7 @@ export const WebsitesInvestigation = () => {
   const onRemove = (id: string) => _remove.mutateAsync(id)
 
   return (
-    <Panel>
+    <>
       <div className="p-1 mb-2">
         {assocationWithClosedCompaniesCount && (
           <Alert dense type={'error'}>
@@ -104,166 +104,168 @@ export const WebsitesInvestigation = () => {
           </Alert>
         )}
       </div>
-      <Datatable
-        id="reportcompanieswebsites"
-        header={
-          <>
-            <DebouncedInput value={_websiteWithCompany.filters.host ?? ''} onChange={onHostChange}>
-              {(value, onChange) => (
-                <InputBase
-                  value={value}
-                  placeholder={m.searchByHost + '...'}
-                  fullWidth
-                  sx={{ml: 1}}
-                  onChange={e => onChange(e.target.value)}
-                />
-              )}
-            </DebouncedInput>
+      <Panel>
+        <Datatable
+          id="reportcompanieswebsites"
+          header={
+            <>
+              <DebouncedInput value={_websiteWithCompany.filters.host ?? ''} onChange={onHostChange}>
+                {(value, onChange) => (
+                  <InputBase
+                    value={value}
+                    placeholder={m.searchByHost + '...'}
+                    fullWidth
+                    sx={{ml: 1}}
+                    onChange={e => onChange(e.target.value)}
+                  />
+                )}
+              </DebouncedInput>
 
-            <DebouncedInput<[Date | undefined, Date | undefined]>
-              value={[_websiteWithCompany.filters.start, _websiteWithCompany.filters.end]}
-              onChange={([start, end]) => {
-                _websiteWithCompany.updateFilters(prev => ({...prev, start, end}))
-              }}
-            >
-              {(value, onChange) => <PeriodPicker value={value} onChange={onChange} sx={{mr: 1}} fullWidth />}
-            </DebouncedInput>
-          </>
-        }
-        actions={
-          <>
-            <Tooltip title={m.removeAllFilters}>
-              <Badge color="error" badgeContent={filtersCount} hidden={filtersCount === 0} overlap="circular">
-                <IconBtn color="primary" onClick={_websiteWithCompany.clearFilters}>
-                  <Icon>clear</Icon>
-                </IconBtn>
-              </Badge>
-            </Tooltip>
-            <WebsitesFilters
-              filters={_websiteWithCompany.filters}
-              updateFilters={_ => {
-                _websiteWithCompany.updateFilters(prev => ({...prev, ..._}))
-              }}
-            >
-              <Tooltip title={m.advancedFilters}>
-                <IconBtn color="primary">
-                  <Icon>filter_list</Icon>
-                </IconBtn>
-              </Tooltip>
-            </WebsitesFilters>
-          </>
-        }
-        loading={_websiteWithCompany.result.isFetching}
-        total={_websiteWithCompany.result.data?.totalCount}
-        paginate={{
-          limit: _websiteWithCompany.filters.limit,
-          offset: _websiteWithCompany.filters.offset,
-          onPaginationChange: pagination => _websiteWithCompany.updateFilters(prev => ({...prev, ...pagination})),
-        }}
-        getRenderRowKey={_ => _.id}
-        data={_websiteWithCompany.result.data?.entities}
-        showColumnsToggle={true}
-        columns={[
-          {
-            id: 'host',
-            head: m.website,
-            render: _ => (
-              <Txt link>
-                <a href={'https://' + _.host}>{_.host}</a>
-              </Txt>
-            ),
-          },
-          {
-            head: m.reports,
-            id: 'reports',
-            render: _ => _.count,
-          },
-          {
-            head: m.association,
-            id: 'association',
-            render: _ => (
-              <SelectWebsiteAssociation
-                website={_}
-                onChange={() => queryClient.invalidateQueries({queryKey: WebsiteWithCompanySearchKeys})}
-              />
-            ),
-          },
-          {
-            head: 'Recherche de Siret',
-            id: 'extraction',
-            render: _ => (
-              <SiretExtraction
-                websiteWithCompany={_}
-                remove={() => onRemove(_.id)}
-                identify={() => queryClient.invalidateQueries({queryKey: WebsiteWithCompanySearchKeys})}
-              />
-            ),
-          },
-          {
-            head: m.firstReportDate,
-            id: 'firstReportDate',
-            render: _ => formatDate(_.creationDate),
-          },
-          {
-            head: m.investigation,
-            id: 'investigationStatus',
-            render: _ => (
-              <AutocompleteDialog<InvestigationStatus>
-                value={_.investigationStatus}
-                title={m.investigation}
-                inputLabel={m.investigation}
-                getOptionLabel={_ => m.InvestigationStatusDesc[_]}
-                options={_investigationStatus.data}
-                onChange={investigationStatus => {
-                  if (_.investigationStatus === investigationStatus) {
-                    toastInfo(m.alreadySelectedValue(investigationStatus))
-                  } else {
-                    _createOrUpdate.mutate({..._, investigationStatus})
-                  }
+              <DebouncedInput<[Date | undefined, Date | undefined]>
+                value={[_websiteWithCompany.filters.start, _websiteWithCompany.filters.end]}
+                onChange={([start, end]) => {
+                  _websiteWithCompany.updateFilters(prev => ({...prev, start, end}))
                 }}
               >
-                <StatusChip
-                  tooltipTitle={m.investigation}
-                  value={_.investigationStatus ? m.InvestigationStatusDesc[_.investigationStatus] : m.noValue}
+                {(value, onChange) => <PeriodPicker value={value} onChange={onChange} sx={{mr: 1}} fullWidth />}
+              </DebouncedInput>
+            </>
+          }
+          actions={
+            <>
+              <Tooltip title={m.removeAllFilters}>
+                <Badge color="error" badgeContent={filtersCount} hidden={filtersCount === 0} overlap="circular">
+                  <IconBtn color="primary" onClick={_websiteWithCompany.clearFilters}>
+                    <Icon>clear</Icon>
+                  </IconBtn>
+                </Badge>
+              </Tooltip>
+              <WebsitesFilters
+                filters={_websiteWithCompany.filters}
+                updateFilters={_ => {
+                  _websiteWithCompany.updateFilters(prev => ({...prev, ..._}))
+                }}
+              >
+                <Tooltip title={m.advancedFilters}>
+                  <IconBtn color="primary">
+                    <Icon>filter_list</Icon>
+                  </IconBtn>
+                </Tooltip>
+              </WebsitesFilters>
+            </>
+          }
+          loading={_websiteWithCompany.result.isFetching}
+          total={_websiteWithCompany.result.data?.totalCount}
+          paginate={{
+            limit: _websiteWithCompany.filters.limit,
+            offset: _websiteWithCompany.filters.offset,
+            onPaginationChange: pagination => _websiteWithCompany.updateFilters(prev => ({...prev, ...pagination})),
+          }}
+          getRenderRowKey={_ => _.id}
+          data={_websiteWithCompany.result.data?.entities}
+          showColumnsToggle={true}
+          columns={[
+            {
+              id: 'host',
+              head: m.website,
+              render: _ => (
+                <Txt link>
+                  <a href={'https://' + _.host}>{_.host}</a>
+                </Txt>
+              ),
+            },
+            {
+              head: m.reports,
+              id: 'reports',
+              render: _ => _.count,
+            },
+            {
+              head: m.association,
+              id: 'association',
+              render: _ => (
+                <SelectWebsiteAssociation
+                  website={_}
+                  onChange={() => queryClient.invalidateQueries({queryKey: WebsiteWithCompanySearchKeys})}
                 />
-              </AutocompleteDialog>
-            ),
-          },
-          {
-            id: 'status',
-            stickyEnd: true,
-            head: m.identified,
-            render: _ => (
-              <Switch
-                checked={_.identificationStatus === IdentificationStatus.Identified}
-                onChange={e =>
-                  handleUpdateKind(_, e.target.checked ? IdentificationStatus.Identified : IdentificationStatus.NotIdentified)
-                }
-              />
-            ),
-          },
+              ),
+            },
+            {
+              head: 'Recherche de Siret',
+              id: 'extraction',
+              render: _ => (
+                <SiretExtraction
+                  websiteWithCompany={_}
+                  remove={() => onRemove(_.id)}
+                  identify={() => queryClient.invalidateQueries({queryKey: WebsiteWithCompanySearchKeys})}
+                />
+              ),
+            },
+            {
+              head: m.firstReportDate,
+              id: 'firstReportDate',
+              render: _ => formatDate(_.creationDate),
+            },
+            {
+              head: m.investigation,
+              id: 'investigationStatus',
+              render: _ => (
+                <AutocompleteDialog<InvestigationStatus>
+                  value={_.investigationStatus}
+                  title={m.investigation}
+                  inputLabel={m.investigation}
+                  getOptionLabel={_ => m.InvestigationStatusDesc[_]}
+                  options={_investigationStatus.data}
+                  onChange={investigationStatus => {
+                    if (_.investigationStatus === investigationStatus) {
+                      toastInfo(m.alreadySelectedValue(investigationStatus))
+                    } else {
+                      _createOrUpdate.mutate({..._, investigationStatus})
+                    }
+                  }}
+                >
+                  <StatusChip
+                    tooltipTitle={m.investigation}
+                    value={_.investigationStatus ? m.InvestigationStatusDesc[_.investigationStatus] : m.noValue}
+                  />
+                </AutocompleteDialog>
+              ),
+            },
+            {
+              id: 'status',
+              stickyEnd: true,
+              head: m.identified,
+              render: _ => (
+                <Switch
+                  checked={_.identificationStatus === IdentificationStatus.Identified}
+                  onChange={e =>
+                    handleUpdateKind(_, e.target.checked ? IdentificationStatus.Identified : IdentificationStatus.NotIdentified)
+                  }
+                />
+              ),
+            },
 
-          {
-            id: 'action',
-            stickyEnd: true,
-            sx: _ => sxUtils.tdActions,
-            render: _ => (
-              <>
-                <WebsiteTools website={_} />
-                {connectedUser.isAdmin ? (
-                  <Tooltip title={m.delete}>
-                    <IconBtn loading={_remove.isPending} color="primary" onClick={() => onRemove(_.id)}>
-                      <Icon>delete</Icon>
-                    </IconBtn>
-                  </Tooltip>
-                ) : (
-                  <></>
-                )}
-              </>
-            ),
-          },
-        ]}
-      />
-    </Panel>
+            {
+              id: 'action',
+              stickyEnd: true,
+              sx: _ => sxUtils.tdActions,
+              render: _ => (
+                <>
+                  <WebsiteTools website={_} />
+                  {connectedUser.isAdmin ? (
+                    <Tooltip title={m.delete}>
+                      <IconBtn loading={_remove.isPending} color="primary" onClick={() => onRemove(_.id)}>
+                        <Icon>delete</Icon>
+                      </IconBtn>
+                    </Tooltip>
+                  ) : (
+                    <></>
+                  )}
+                </>
+              ),
+            },
+          ]}
+        />
+      </Panel>
+    </>
   )
 }
