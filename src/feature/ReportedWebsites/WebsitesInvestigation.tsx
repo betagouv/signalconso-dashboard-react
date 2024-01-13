5c93f5312debfa1b9047c2d4b6353fc11f6518f6
ssedoudbgouv <s.sedoud.betagouv@gmail.com>
2024-01-13 14:33:03 +0100

    TRELLO-2010: fix bad rebase

--- src/feature/ReportedWebsites/WebsitesInvestigation.tsx
+++ src/feature/ReportedWebsites/WebsitesInvestigation.tsx
@@ -1,12 +1,11 @@
 import React, {useCallback, useMemo, useState} from 'react'
 import {useI18n} from '../../core/i18n'
-import {Badge, Box, Icon, InputBase, Switch, Tooltip} from '@mui/material'
-import {Alert} from '../../alexlibs/mui-extension'
+import {Badge, Icon, InputBase, Switch, Tooltip} from '@mui/material'
 import {useToast} from '../../core/toast'
 import {Panel} from '../../shared/Panel'
 import {Datatable} from '../../shared/Datatable/Datatable'
 import {DebouncedInput} from '../../shared/DebouncedInput'
-import {Btn, IconBtn, Txt} from '../../alexlibs/mui-extension'
+import {Alert, Btn, IconBtn, Txt} from '../../alexlibs/mui-extension'
 import {StatusChip} from './StatusChip'
 import {WebsitesFilters} from './WebsitesFilters'
 import {SelectWebsiteAssociation} from './SelectWebsiteIdentification/SelectWebsiteAssociation'
@@ -21,11 +20,12 @@ import {PeriodPicker} from '../../shared/PeriodPicker'
 import {SiretExtraction} from './SiretExtraction'
 import {
   useListInvestigationStatusQuery,
-  useWebsiteWithClosedCompanyQuery,
   useWebsiteWithCompanySearchQuery,
+  useWebsiteWithClosedCompanyQuery,
   WebsiteWithCompanySearchKeys,
 } from '../../core/queryhooks/websiteQueryHooks'
 import {useMutation, useQueryClient} from '@tanstack/react-query'
+import {useEffectFn} from '../../alexlibs/react-hooks-lib'
 
 export const WebsitesInvestigation = () => {
   const {m, formatDate} = useI18n()
@@ -54,33 +54,6 @@ export const WebsitesInvestigation = () => {
   })
   const {toastInfo, toastSuccess} = useToast()
 
-<<<<<<< Updated upstream
-=======
-  const websitesIndex = useMap<Id, WebsiteWithCompany>()
-
-  const [assocationWithClosedCompaniesCount, setAssocationWithClosedCompaniesCount] = useState<number | undefined>(undefined)
-
-  useEffectFn(_websiteWithCompany.result.data, w => {
-    websitesIndex.clear()
-    w.entities.map(_ => websitesIndex.set(_.id, _))
-  })
-
-  useEffectFn(_websiteWithClosedCompany.result.data, w => {
-    setAssocationWithClosedCompaniesCount(w.totalCount)
-  })
-
-  const handleSeeClosedCompanyAssociation = () => {
-    _websiteWithCompany.clearFilters()
-    _websiteWithCompany.updateFilters(prev => ({
-      ...prev,
-      ...{
-        isOpen: false,
-        identificationStatus: [IdentificationStatus.Identified],
-      },
-    }))
-  }
-
->>>>>>> Stashed changes
   const handleUpdateKind = (website: WebsiteWithCompany, identificationStatus: IdentificationStatus) => {
     _updateStatus.mutate({id: website.id, identificationStatus})
   }
@@ -98,11 +71,28 @@ export const WebsitesInvestigation = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
+  const [assocationWithClosedCompaniesCount, setAssocationWithClosedCompaniesCount] = useState<number | undefined>(undefined)
+
+  useEffectFn(_websiteWithClosedCompany.result.data, w => {
+    setAssocationWithClosedCompaniesCount(w.totalCount)
+  })
+
+  const handleSeeClosedCompanyAssociation = () => {
+    _websiteWithCompany.clearFilters()
+    _websiteWithCompany.updateFilters(prev => ({
+      ...prev,
+      ...{
+        isOpen: false,
+        identificationStatus: [IdentificationStatus.Identified],
+      },
+    }))
+  }
+
   const onRemove = (id: string) => _remove.mutateAsync(id)
 
   return (
-    <>
-      <Box sx={{p: 1, mb: 2}}>
+    <Panel>
+      <div className="p-1 mb-2">
         {assocationWithClosedCompaniesCount && (
           <Alert dense type={'error'}>
             {m.websiteInvestigationClosedCompanyAssociationDesc}
@@ -113,9 +103,23 @@ export const WebsitesInvestigation = () => {
             </div>
           </Alert>
         )}
-      </Box>
+      </div>
+      <Datatable
+        id="reportcompanieswebsites"
+        header={
+          <>
+            <DebouncedInput value={_websiteWithCompany.filters.host ?? ''} onChange={onHostChange}>
+              {(value, onChange) => (
+                <InputBase
+                  value={value}
+                  placeholder={m.searchByHost + '...'}
+                  fullWidth
+                  sx={{ml: 1}}
+                  onChange={e => onChange(e.target.value)}
+                />
+              )}
+            </DebouncedInput>
 
-<<<<<<< Updated upstream
             <DebouncedInput<[Date | undefined, Date | undefined]>
               value={[_websiteWithCompany.filters.start, _websiteWithCompany.filters.end]}
               onChange={([start, end]) => {
@@ -216,171 +220,50 @@ export const WebsitesInvestigation = () => {
                   } else {
                     _createOrUpdate.mutate({..._, investigationStatus})
                   }
-=======
-      <Panel>
-        <Datatable
-          id="reportcompanieswebsites"
-          header={
-            <>
-              <DebouncedInput value={_websiteWithCompany.filters.host ?? ''} onChange={onHostChange}>
-                {(value, onChange) => (
-                  <InputBase
-                    value={value}
-                    placeholder={m.searchByHost + '...'}
-                    fullWidth
-                    sx={{ml: 1}}
-                    onChange={e => onChange(e.target.value)}
-                  />
+                }}
+              >
+                <StatusChip
+                  tooltipTitle={m.investigation}
+                  value={_.investigationStatus ? m.InvestigationStatusDesc[_.investigationStatus] : m.noValue}
+                />
+              </AutocompleteDialog>
+            ),
+          },
+          {
+            id: 'status',
+            stickyEnd: true,
+            head: m.identified,
+            render: _ => (
+              <Switch
+                checked={_.identificationStatus === IdentificationStatus.Identified}
+                onChange={e =>
+                  handleUpdateKind(_, e.target.checked ? IdentificationStatus.Identified : IdentificationStatus.NotIdentified)
+                }
+              />
+            ),
+          },
+
+          {
+            id: 'action',
+            stickyEnd: true,
+            sx: _ => sxUtils.tdActions,
+            render: _ => (
+              <>
+                <WebsiteTools website={_} />
+                {connectedUser.isAdmin ? (
+                  <Tooltip title={m.delete}>
+                    <IconBtn loading={_remove.isPending} color="primary" onClick={() => onRemove(_.id)}>
+                      <Icon>delete</Icon>
+                    </IconBtn>
+                  </Tooltip>
+                ) : (
+                  <></>
                 )}
-              </DebouncedInput>
-
-              <DebouncedInput<[Date | undefined, Date | undefined]>
-                value={[_websiteWithCompany.filters.start, _websiteWithCompany.filters.end]}
-                onChange={([start, end]) => {
-                  _websiteWithCompany.updateFilters(prev => ({...prev, start, end}))
->>>>>>> Stashed changes
-                }}
-              >
-                {(value, onChange) => <PeriodPicker value={value} onChange={onChange} sx={{mr: 1}} fullWidth />}
-              </DebouncedInput>
-            </>
-          }
-          actions={
-            <>
-              <Tooltip title={m.removeAllFilters}>
-                <Badge color="error" badgeContent={filtersCount} hidden={filtersCount === 0} overlap="circular">
-                  <IconBtn color="primary" onClick={_websiteWithCompany.clearFilters}>
-                    <Icon>clear</Icon>
-                  </IconBtn>
-                </Badge>
-              </Tooltip>
-              <WebsitesFilters
-                filters={_websiteWithCompany.filters}
-                updateFilters={_ => {
-                  _websiteWithCompany.updateFilters(prev => ({...prev, ..._}))
-                }}
-              >
-                <Tooltip title={m.advancedFilters}>
-                  <IconBtn color="primary">
-                    <Icon>filter_list</Icon>
-                  </IconBtn>
-                </Tooltip>
-              </WebsitesFilters>
-            </>
-          }
-          loading={_websiteWithCompany.result.isFetching}
-          total={_websiteWithCompany.result.data?.totalCount}
-          paginate={{
-            limit: _websiteWithCompany.filters.limit,
-            offset: _websiteWithCompany.filters.offset,
-            onPaginationChange: pagination => _websiteWithCompany.updateFilters(prev => ({...prev, ...pagination})),
-          }}
-          getRenderRowKey={_ => _.id}
-          data={websitesIndex.values()}
-          showColumnsToggle={true}
-          columns={[
-            {
-              id: 'host',
-              head: m.website,
-              render: _ => (
-                <Txt link>
-                  <a href={'https://' + _.host}>{_.host}</a>
-                </Txt>
-              ),
-            },
-            {
-              head: m.reports,
-              id: 'reports',
-              render: _ => _.count,
-            },
-            {
-              head: m.association,
-              id: 'association',
-              render: _ => (
-                <SelectWebsiteAssociation
-                  website={_}
-                  onChange={() => queryClient.invalidateQueries({queryKey: WebsiteWithCompanySearchKeys})}
-                />
-              ),
-            },
-            {
-              head: 'Recherche de Siret',
-              id: 'extraction',
-              render: _ => (
-                <SiretExtraction
-                  websiteWithCompany={_}
-                  remove={() => onRemove(_.id)}
-                  identify={() => queryClient.invalidateQueries({queryKey: WebsiteWithCompanySearchKeys})}
-                />
-              ),
-            },
-            {
-              head: m.firstReportDate,
-              id: 'firstReportDate',
-              render: _ => formatDate(_.creationDate),
-            },
-            {
-              head: m.investigation,
-              id: 'investigationStatus',
-              render: _ => (
-                <AutocompleteDialog<InvestigationStatus>
-                  value={_.investigationStatus}
-                  title={m.investigation}
-                  inputLabel={m.investigation}
-                  getOptionLabel={_ => m.InvestigationStatusDesc[_]}
-                  options={_investigationStatus.data}
-                  onChange={investigationStatus => {
-                    if (_.investigationStatus === investigationStatus) {
-                      toastInfo(m.alreadySelectedValue(investigationStatus))
-                    } else {
-                      _createOrUpdate.mutate({..._, investigationStatus})
-                      websitesIndex.set(_.id, {..._, lastUpdated: new Date(Date.now()), investigationStatus})
-                    }
-                  }}
-                >
-                  <StatusChip
-                    tooltipTitle={m.investigation}
-                    value={_.investigationStatus ? m.InvestigationStatusDesc[_.investigationStatus] : m.noValue}
-                  />
-                </AutocompleteDialog>
-              ),
-            },
-            {
-              id: 'status',
-              stickyEnd: true,
-              head: m.identified,
-              render: _ => (
-                <Switch
-                  checked={_.identificationStatus === IdentificationStatus.Identified}
-                  onChange={e =>
-                    handleUpdateKind(_, e.target.checked ? IdentificationStatus.Identified : IdentificationStatus.NotIdentified)
-                  }
-                />
-              ),
-            },
-
-            {
-              id: 'action',
-              stickyEnd: true,
-              sx: _ => sxUtils.tdActions,
-              render: _ => (
-                <>
-                  <WebsiteTools website={_} />
-                  {connectedUser.isAdmin ? (
-                    <Tooltip title={m.delete}>
-                      <IconBtn loading={_remove.isPending} color="primary" onClick={() => onRemove(_.id)}>
-                        <Icon>delete</Icon>
-                      </IconBtn>
-                    </Tooltip>
-                  ) : (
-                    <></>
-                  )}
-                </>
-              ),
-            },
-          ]}
-        />
-      </Panel>
-    </>
+              </>
+            ),
+          },
+        ]}
+      />
+    </Panel>
   )
 }
