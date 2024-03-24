fe5e4c6fbcab78e9015bcaa8695aa97e9167de7e
Said SEDOUD <s.sedoud.betagouv@gmail.com>
2024-03-24 08:52:09 +0100

    TRELLO-1403 : refacto & format

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,22 @@
-import {CircularProgress, CssBaseline, StyledEngineProvider, ThemeProvider} from '@mui/material'
+import {CssBaseline, StyledEngineProvider, ThemeProvider} from '@mui/material'
 import {QueryClientProvider} from '@tanstack/react-query'
-import {ApiProvider} from 'core/context/ApiContext'
-import {LoginForm} from 'feature/Login/LoginForm'
-import {RegisterForm} from 'feature/Login/RegisterForm'
-import {WelcomePage} from 'feature/Login/WelcomePage'
-import React, {ReactNode, useEffect} from 'react'
-import {Navigate, Routes, useLocation, useNavigate, useParams} from 'react-router'
-import {BrowserRouter, HashRouter, Route} from 'react-router-dom'
+import React, {useEffect} from 'react'
+import {useNavigate} from 'react-router'
+import {BrowserRouter, HashRouter} from 'react-router-dom'
 import {ToastProvider} from './alexlibs/mui-extension'
 import {config} from './conf/config'
-import {apiPublicSdk, makeSecuredSdk} from './core/ApiSdkInstance'
+import {apiPublicSdk} from './core/ApiSdkInstance'
 import {Layout} from './core/Layout'
 import {ScHeader} from './core/ScHeader/ScHeader'
 import {ScSidebar} from './core/ScSidebar/ScSidebar'
-import {LoginProvider, useLogin} from './core/context/LoginContext'
 import {I18nProvider} from './core/i18n'
-import {Matomo} from './core/plugins/Matomo'
-import {siteMap} from './core/siteMap'
 import {muiTheme} from './core/theme'
 import {useToast} from './core/toast'
-import {AddCompanyForm} from './feature/AddCompany/AddCompanyForm'
-import {Tools} from './feature/AdminTools/Tools'
-import {Companies} from './feature/Companies/Companies'
-import {CompaniesPro} from './feature/CompaniesPro/CompaniesPro'
-import {CompanyComponent} from './feature/Company/Company'
-import {CompanyAccesses} from './feature/CompanyAccesses/CompanyAccesses'
-import {EmailValidation} from './feature/EmailValidation/EmailValidation'
-import {JoinNewsletter} from './feature/JoinNewsletter/JoinNewsletter'
-import {ModeEmploiDGCCRF} from './feature/ModeEmploiDGCCRF/ModeEmploiDGCCRF'
-import {ReportComponent} from './feature/Report/Report'
-import {ReportedPhones} from './feature/ReportedPhones/ReportedPhones'
-import {ReportedWebsites} from './feature/ReportedWebsites/ReportedWebsites'
-import {Reports} from './feature/Reports/Reports'
-import {ReportsPro} from './feature/ReportsPro/ReportsPro'
-import {ResetPassword} from './feature/ResetPassword/ResetPassword'
-import {Settings} from './feature/Settings/Settings'
-import {Stats} from './feature/Stats/Stats'
-import {Subscriptions} from './feature/Subscriptions/Subscriptions'
-import {UserActivation} from './feature/Users/UserActivation'
-import {Users} from './feature/Users/Users'
-import {CenteredContent} from './shared/CenteredContent'
 import {Login} from './shared/Login'
 import {Provide} from './shared/Provide'
 import './style.css'
-import {UpdateEmail} from './feature/Settings/UpdateEmail'
 import {queryClient, setQueryClientErrorHandler} from 'queryClient'
-import {ReportPro} from './feature/Report/ReportPro'
+import {AppRoutes} from './AppRoutes'
 
 const Router: typeof HashRouter = config.useHashRouter ? HashRouter : BrowserRouter
 
@@ -61,11 +32,12 @@ export const App = () => {
         _ => <ToastProvider horizontal="right" children={_} />,
       ]}
     >
-      <AppRoutes />
+      <App_ />
     </Provide>
   )
 }
-const AppRoutes = () => {
+
+const App_ = () => {
   const navigate = useNavigate()
   const {toastError} = useToast()
 
@@ -85,110 +57,21 @@ const AppRoutes = () => {
       getUser={apiPublicSdk.authenticate.getUser}
     >
       {({authResponse, login, logout, register, setUser, isFetchingUser}) => {
-        const userActivation = (
-          <UserActivation
-            onActivateUser={apiPublicSdk.user.activateAccount}
-            onFetchTokenInfo={apiPublicSdk.user.fetchTokenInfo}
-          />
-        )
-
         return (
           <Layout header={<ScHeader />} sidebar={authResponse && <ScSidebar connectedUser={authResponse} logout={logout} />}>
             <QueryClientProvider client={queryClient}>
-              <Routes>
-                <Route
-                  path={siteMap.loggedout.emailValidation}
-                  element={<EmailValidation onSaveUser={setUser} onValidateEmail={apiPublicSdk.authenticate.validateEmail} />}
-                />
-                <Route
-                  path={siteMap.loggedout.resetPassword()}
-                  element={<ResetPassword onResetPassword={apiPublicSdk.authenticate.resetPassword} />}
-                />
-                <Route path={siteMap.loggedout.activatePro()} element={userActivation} />
-                <Route path={siteMap.loggedout.activateAdmin} element={userActivation} />
-                <Route path={siteMap.loggedout.activateAgent} element={userActivation} />
-                <Route path={siteMap.loggedout.consumerReview()} element={userActivation} />
-
-                <Route
-                  path="*"
-                  element={
-                    authResponse ? (
-                      <LoginProvider
-                        connectedUser={authResponse}
-                        setConnectedUser={setUser}
-                        onLogout={logout}
-                        apiSdk={makeSecuredSdk()}
-                      >
-                        <ProtectedRoutes />
-                      </LoginProvider>
-                    ) : isFetchingUser ? (
-                      <CenteredContent>
-                        <CircularProgress />
-                      </CenteredContent>
-                    ) : (
-                      <Routes>
-                        <Route path={siteMap.loggedout.register} element={<RegisterForm {...{register}} />} />
-                        <Route path={siteMap.loggedout.login} element={<LoginForm {...{login}} />} />
-                        <Route path="/*" element={<WelcomePage />} />
-                      </Routes>
-                    )
-                  }
-                />
-              </Routes>
+              <AppRoutes
+                authResponse={authResponse}
+                setUser={setUser}
+                register={register}
+                isFetchingUser={isFetchingUser}
+                login={login}
+                logout={logout}
+              />
             </QueryClientProvider>
           </Layout>
         )
       }}
     </Login>
-  )
-}
-
-const ProtectedRoutes = () => {
-  const {apiSdk, connectedUser} = useLogin()
-
-  const location = useLocation()
-  useEffect(() => {
-    Matomo.trackPage(`/${connectedUser.role.toLocaleLowerCase()}${location.pathname}`)
-  }, [location])
-
-  return (
-    <ApiProvider api={apiSdk}>
-      <Routes>
-        <Route path={siteMap.logged.tools.value} element={<Tools />} />
-        <Route path={siteMap.logged.reportedWebsites.value} element={<ReportedWebsites />} />
-        <Route path={siteMap.logged.reportedPhone} element={<ReportedPhones />} />
-        <Route
-          path={siteMap.logged.reportsfiltred.closed}
-          //Keep the key property unique to this route
-          //When using React Router v6 and you have multiple routes pointing to the same component (here siteMap.logged.reportsfiltred.closed and siteMap.logged.report()),
-          //by default, React Router does not re-render the component if it's already mounted and the route changes to another path that uses the same component.
-          //We have to add a key properties to distinguish the two calls and force re-render
-          element={<ReportsPro reportType="closed" key={siteMap.logged.reportsfiltred.closed} />}
-        />
-        <Route path={siteMap.logged.report()} element={connectedUser.isPro ? <ReportPro /> : <ReportComponent />} />
-        <Route
-          path={siteMap.logged.reports()}
-          //Keep the key property unique to this route ( see comment above )
-          element={connectedUser.isPro ? <ReportsPro reportType="open" key={siteMap.logged.reports()} /> : <Reports />}
-        />
-        <Route path={siteMap.logged.users.value()} element={<Users />} />
-        <Route path={siteMap.logged.companies.value} element={<Companies />} />
-        <Route path={siteMap.logged.companyAccesses()} element={<CompanyAccesses />} />
-        <Route path={siteMap.logged.company(':id')} element={<CompanyComponent />} />
-        <Route path={siteMap.logged.subscriptions} element={<Subscriptions />} />
-        <Route path={siteMap.logged.companiesPro} element={<CompaniesPro />} />
-        <Route path={siteMap.logged.joinInformation} element={<JoinNewsletter />} />
-        <Route path={siteMap.logged.updateEmail(':token')} element={<UpdateEmail />} />
-        <Route path={siteMap.logged.settings} element={<Settings />} />
-
-        <Route
-          path={siteMap.logged.modeEmploiDGCCRF}
-          element={connectedUser.isPro ? <ReportsPro reportType="open" /> : <ModeEmploiDGCCRF />}
-        />
-        <Route path={siteMap.logged.stats.value} element={<Stats />} />
-        <Route path={siteMap.loggedout.register} element={<AddCompanyForm />} />
-        <Route path="/*" element={<Navigate replace to={siteMap.logged.reports()} />} />
-      </Routes>
-    </ApiProvider>
   )
 }
