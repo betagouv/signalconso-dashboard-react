bf5835feb829bc377175fc8752f591b1537125bf
eletallbetagouv <107104509+eletallbetagouv@users.noreply.github.com>
2024-04-01 19:39:32 +0200

    improve wordings for "courrier retourne" popin

--- src/feature/CompanyAccesses/SaveUndeliveredDocBtn.tsx
+++ src/feature/CompanyAccesses/SaveUndeliveredDocBtn.tsx
@@ -1,11 +1,11 @@
-import {ScButton} from '../../shared/Button'
-import {useI18n} from '../../core/i18n'
-import React, {useState} from 'react'
-import {useLogin} from '../../core/context/LoginContext'
-import {Datepicker} from '../../shared/Datepicker'
-import {useToast} from '../../core/toast'
-import {ScDialog} from '../../shared/ScDialog'
 import {ButtonProps} from '@mui/material'
+import {useState} from 'react'
+import {useLogin} from '../../core/context/LoginContext'
+import {useI18n} from '../../core/i18n'
+import {useToast} from '../../core/toast'
+import {ScButton} from '../../shared/Button'
+import {Datepicker} from '../../shared/Datepicker'
+import {ScDialog} from '../../shared/ScDialog'
 
 interface Props extends Omit<ButtonProps, 'onChange'> {
   loading: boolean
@@ -25,7 +25,7 @@ export const SaveUndeliveredDocBtn = ({loading, onChange, ...props}: Props) => {
     <ScDialog
       title={m.undeliveredDocTitle}
       loading={loading}
-      confirmLabel={m.confirm}
+      confirmLabel={'Enregistrer'}
       confirmDisabled={!returnDate}
       onConfirm={(event, close) =>
         onChange(returnDate)
@@ -34,6 +34,12 @@ export const SaveUndeliveredDocBtn = ({loading, onChange, ...props}: Props) => {
       }
       content={
         <>
+          <p className="mb-4">
+            Quand un courrier d'activation destiné à cette entreprise nous a été retourné (mauvaise adresse, refus, etc.), vous
+            pouvez l'indiquer ici.
+          </p>
+          <p className="mb-4">Cela permet de garder une trace de cet évènement.</p>
+
           <Datepicker
             sx={{mr: 1}}
             fullWidth
