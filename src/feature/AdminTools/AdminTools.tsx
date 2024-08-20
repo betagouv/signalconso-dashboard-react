82bd497dad5f56bb3d741b3929688c9ca1cf35c3
Charles Dufour <charles.dufour@beta.gouv.fr>
2024-08-20 14:02:49 +0200

    [TRELLO-2464] Add screen to manage blacklisted ips

--- src/feature/AdminTools/AdminTools.tsx
+++ src/feature/AdminTools/AdminTools.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import {ResendEmailsAdminTool} from './ResendEmailAdminTool'
 import {DeleteSpamReportsAdminTool} from './DeleteSpamReportsAdminTool'
+import {BlacklistedIpsTool} from './BlacklistedIpsTool'
 
 export const AdminTools = () => {
   return (
     <>
       <ResendEmailsAdminTool />
       <DeleteSpamReportsAdminTool />
+      <BlacklistedIpsTool />
     </>
   )
 }
