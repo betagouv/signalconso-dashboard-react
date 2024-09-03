02965945f6b6455602c8dd6222d741d40c5eb237
eletallbetagouv <107104509+eletallbetagouv@users.noreply.github.com>
2024-09-03 15:49:38 +0200

    simplify Fender props and remove any

--- src/alexlibs/mui-extension/color.ts
+++ src/alexlibs/mui-extension/color.ts
@@ -3,4 +3,5 @@ export const colorBlueFrance = '#000091'
 export const colorSuccess = '#119647',
   colorError = '#ea1000',
   colorWarning = '#FF8000',
-  colorInfo = '#e3e3fd'
+  colorInfo = '#e3e3fd',
+  colorGray = '#9e9e9e'
