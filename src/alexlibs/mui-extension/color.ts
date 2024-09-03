bac5a73daedc32413d32097bdf860495bde38f28
eletallbetagouv <107104509+eletallbetagouv@users.noreply.github.com>
2024-09-03 16:36:58 +0200

    avoid using t: any in Txt for colors

--- src/alexlibs/mui-extension/color.ts
+++ src/alexlibs/mui-extension/color.ts
@@ -5,3 +5,13 @@ export const colorSuccess = '#119647',
   colorWarning = '#FF8000',
   colorInfo = '#e3e3fd',
   colorGray = '#9e9e9e'
+
+export const colorMuiTextSecondary = 'rgba(0, 0, 0, 0.6)'
+export const colorMuiTextDisabled = 'rgba(0, 0, 0, 0.38)'
+export const colorsMui = {
+  textSecondary: 'rgba(0, 0, 0, 0.6)',
+  textDisabled: 'rgba(0, 0, 0, 0.38)',
+  errorMain: '#d32f2f',
+  successMain: '#2e7d32',
+  warningMain: '#ed6c02',
+}
