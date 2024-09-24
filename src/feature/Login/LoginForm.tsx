ef2ded84356934550cf0979e10338cf55093bf76
eletallbetagouv <107104509+eletallbetagouv@users.noreply.github.com>
2024-09-24 13:55:58 +0300

    handle SC-0013 with a dedicated message

--- src/feature/Login/LoginForm.tsx
+++ src/feature/Login/LoginForm.tsx
@@ -3,12 +3,18 @@ import {
   AlertContactSupport,
   EspaceProTitle,
 } from 'feature/Login/loggedOutComponents'
+import { useState } from 'react'
 import { useForm } from 'react-hook-form'
+import { useNavigate } from 'react-router'
 import { CenteredContent } from 'shared/CenteredContent'
 import { Alert, Txt } from '../../alexlibs/mui-extension'
 import { ApiError } from '../../core/client/ApiClient'
 import { SignalConsoPublicSdk } from '../../core/client/SignalConsoPublicSdk'
 import { regexp } from '../../core/helper/regexp'
+import {
+  mapArrayFromQuerystring,
+  useQueryString,
+} from '../../core/helper/useQueryString'
 import { useI18n } from '../../core/i18n'
 import {
   AuthenticationEventActions,
@@ -16,14 +22,9 @@ import {
   Matomo,
 } from '../../core/plugins/Matomo'
 import { ScButton } from '../../shared/Button'
+import { InfoBanner } from '../../shared/InfoBanner'
 import { ScInputPassword } from '../../shared/ScInputPassword'
 import { ForgottenPasswordDialog } from './ForgottenPasswordDialog'
-import { useNavigate } from 'react-router'
-import {
-  mapArrayFromQuerystring,
-  useQueryString,
-} from '../../core/helper/useQueryString'
-import { InfoBanner } from '../../shared/InfoBanner'
 
 interface ActionProps<F extends (...args: any[]) => Promise<any>> {
   action: F
@@ -57,15 +58,15 @@ export const LoginForm = ({ login }: Props) => {
     toQueryString: (_) => _,
     fromQueryString: mapArrayFromQuerystring(['redirecturl']),
   })
-
   const {
     register,
     handleSubmit,
     watch,
-    setError,
     clearErrors,
     formState: { errors },
   } = useForm<Form>({ mode: 'onSubmit' })
+  const [apiError, setApiError] = useState<ApiError | undefined>()
+  const needEmailRevalidationApiError = apiError?.details.id === 'SC-0013'
 
   const onLogin = async (form: Form) => {
     login
@@ -87,10 +88,7 @@ export const LoginForm = ({ login }: Props) => {
         }
       })
       .catch((err: ApiError) => {
-        setError('apiError', {
-          type: err.details.id,
-          message: err.message,
-        })
+        setApiError(err)
         Matomo.trackEvent(EventCategories.auth, AuthenticationEventActions.fail)
       })
   }
@@ -100,58 +98,72 @@ export const LoginForm = ({ login }: Props) => {
       <InfoBanner />
       <EspaceProTitle subPageTitle={m.login} />
       <div className="w-full max-w-xl">
-        <form
-          className="flex flex-col mb-8"
-          onSubmit={handleSubmit(onLogin)}
-          action="#"
-        >
-          {errors.apiError && (
-            <Alert type="error" sx={{ mb: 2 }}>
-              <Txt size="big" block bold>
-                {m.somethingWentWrong}
-              </Txt>
-              <Txt>{errors.apiError.message}</Txt>
-            </Alert>
-          )}
-          <TextField
-            autoComplete="username"
-            type="email"
-            variant="filled"
-            error={!!errors.email}
-            helperText={errors.email?.message ?? ' '}
-            label={m.yourEmail}
-            {...register('email', {
-              required: { value: true, message: m.required },
-              pattern: { value: regexp.email, message: m.invalidEmail },
-            })}
-          />
-          <ScInputPassword
-            autoComplete="current-password"
-            label={m.password}
-            error={!!errors.password}
-            helperText={errors.password?.message ?? ' '}
-            {...register('password', {
-              required: { value: true, message: m.required },
-            })}
-          />
-          <div className="flex gap-4 items-center justify-center">
-            <ForgottenPasswordDialog value={watch('email')}>
-              <ScButton color="primary" size="large" variant="text">
-                {m.forgottenPassword}
+        {needEmailRevalidationApiError ? (
+          <Alert type="error" sx={{ mb: 2 }}>
+            <p className="mb-2">
+              Votre adresse email <strong>{watch('email')}</strong> a besoin
+              d'être revalidée.
+            </p>
+            <p>
+              Nous vous avons envoyé un email, cliquez sur le lien qu'il
+              contient pour revalider votre adresse et vous connecter à
+              SignalConso.
+            </p>
+          </Alert>
+        ) : (
+          <form
+            className="flex flex-col mb-8"
+            onSubmit={handleSubmit(onLogin)}
+            action="#"
+          >
+            {apiError && (
+              <Alert type="error" sx={{ mb: 2 }}>
+                <Txt size="big" block bold>
+                  {m.somethingWentWrong}
+                </Txt>
+                <Txt>{apiError.message}</Txt>
+              </Alert>
+            )}
+            <TextField
+              autoComplete="username"
+              type="email"
+              variant="filled"
+              error={!!errors.email}
+              helperText={errors.email?.message ?? ' '}
+              label={m.yourEmail}
+              {...register('email', {
+                required: { value: true, message: m.required },
+                pattern: { value: regexp.email, message: m.invalidEmail },
+              })}
+            />
+            <ScInputPassword
+              autoComplete="current-password"
+              label={m.password}
+              error={!!errors.password}
+              helperText={errors.password?.message ?? ' '}
+              {...register('password', {
+                required: { value: true, message: m.required },
+              })}
+            />
+            <div className="flex gap-4 items-center justify-center">
+              <ForgottenPasswordDialog value={watch('email')}>
+                <ScButton color="primary" size="large" variant="text">
+                  {m.forgottenPassword}
+                </ScButton>
+              </ForgottenPasswordDialog>
+              <ScButton
+                loading={login.loading}
+                type="submit"
+                onClick={(_) => clearErrors('apiError')}
+                variant="contained"
+                color="primary"
+                size="large"
+              >
+                {m.imLoggingIn}
               </ScButton>
-            </ForgottenPasswordDialog>
-            <ScButton
-              loading={login.loading}
-              type="submit"
-              onClick={(_) => clearErrors('apiError')}
-              variant="contained"
-              color="primary"
-              size="large"
-            >
-              {m.imLoggingIn}
-            </ScButton>
-          </div>
-        </form>
+            </div>
+          </form>
+        )}
         <AlertContactSupport />
       </div>
     </CenteredContent>
