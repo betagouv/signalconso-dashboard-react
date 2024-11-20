05da49ea0c0b60e4eef73bafe701f905456a1de6
eletallbetagouv <107104509+eletallbetagouv@users.noreply.github.com>
2024-11-20 11:35:18 +0200

    remove unused inexact type

--- src/core/client/authenticate/Authenticate.ts
+++ src/core/client/authenticate/Authenticate.ts
@@ -1,14 +1,7 @@
 export type ValidationRejectReason = 'TOO_MANY_ATTEMPTS' | 'INVALID_CODE'
 
-enum TokenKind {
-  companyInit = 'COMPANY_INIT',
-  companyJoin = 'COMPANY_JOIN',
-  dgccrfAccount = 'DGCCRF_ACCOUNT',
-  dgalAccount = 'DGAL_ACCOUNT',
-}
-
 export interface TokenInfo {
   token: string
-  kind: TokenKind
+  kind: unknown
   emailedTo: string
 }
