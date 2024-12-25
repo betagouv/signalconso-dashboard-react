e7d2b71984a02492ae77017ec585227ffba4dd21
eletallbetagouv <107104509+eletallbetagouv@users.noreply.github.com>
2024-12-25 17:55:57 +0200

    ui: simplify horizontalbarchart

--- src/shared/Chart/HorizontalBarChart.tsx
+++ src/shared/Chart/HorizontalBarChart.tsx
@@ -1,11 +1,9 @@
-import { alpha, Box, Theme, Tooltip } from '@mui/material'
+import { Theme, Tooltip } from '@mui/material'
 import withStyles from '@mui/styles/withStyles'
 import { mapFor } from 'core/helper'
 import { ReactNode, useMemo, useState } from 'react'
-import { makeSx, Txt } from '../../alexlibs/mui-extension'
 import { useTimeout } from '../../alexlibs/react-hooks-lib'
 import { useI18n } from '../../core/i18n'
-import { styleUtils } from '../../core/theme'
 
 export interface HorizontalBarChartData {
   label: ReactNode
@@ -16,26 +14,10 @@ export interface HorizontalBarChartData {
 interface Props {
   data?: HorizontalBarChartData[]
   grid?: boolean
-  width?: number
 }
 
-const sx = makeSx({
-  item: {
-    display: 'flex',
-    my: 0.5,
-    mx: 0,
-  },
-  label: {
-    alignSelf: 'flex-end',
-    textAlign: 'right',
-    p: 0,
-    pr: 2,
-    textOverflow: 'ellipsis',
-    whiteSpace: 'nowrap',
-  },
-})
-
-export const HorizontalBarChart = ({ data, grid, width = 200 }: Props) => {
+export const HorizontalBarChart = ({ data, grid }: Props) => {
+  const firstColumnWidth = 200
   const { m } = useI18n()
   const maxValue = useMemo(
     () => data && Math.max(...data.map((_) => _.value)),
@@ -46,108 +28,83 @@ export const HorizontalBarChart = ({ data, grid, width = 200 }: Props) => {
     [data],
   )
   const [appeared, setAppeared] = useState<boolean>(false)
-  const gridAxis = 4
+  const nbGridColumns = 8
   const { formatLargeNumber } = useI18n()
   useTimeout(() => setAppeared(true), 0)
 
   return (
-    <Box
-      sx={{
-        overflow: 'hidden',
-      }}
-    >
+    <div className="overflow-hidden">
       {data && maxValue && sumValue ? (
-        data.map((item, i) => {
+        data.map((item, i: number) => {
           const percentOfMax = (item.value / maxValue) * 100
           const percentOfAll = (item.value / sumValue) * 100
           return (
-            <Box key={i} sx={sx.item}>
-              <Box sx={sx.label} style={{ width: width, minWidth: width }}>
+            <div key={i} className="flex my-2 hover:bg-gray-200">
+              <div
+                className="pr-2 text-right overflow-ellipsis whitespace-nowrap"
+                style={{ width: firstColumnWidth, minWidth: firstColumnWidth }}
+              >
                 {item.label}
-              </Box>
+              </div>
               <LightTooltip
                 title={
-                  <>
-                    <Txt size="big" block bold>
-                      {item.label}
-                    </Txt>
-                    <div style={{ textAlign: 'right' }}>
-                      <Txt size="title" color="primary" block>
-                        {formatLargeNumber(item.value)}
-                      </Txt>
-                      <Txt size="big" color="hint" block>
-                        {Math.ceil(percentOfAll)}%
-                      </Txt>
+                  <div>
+                    <div className="text-lg">{item.label}</div>
+                    <div className="text-right text-scbluefrance">
+                      <p className="text-xl font-bold">
+                        {formatLargeNumber(item.value)}{' '}
+                        <span className="font-normal text-base">
+                          ({Math.ceil(percentOfAll)}%)
+                        </span>
+                      </p>
                     </div>
-                  </>
+                  </div>
                 }
               >
-                <Box
-                  sx={{
-                    py: 0.25,
-                    px: 0,
-                    flex: 1,
-                    transition: (t) => t.transitions.create('background'),
-                    '&:hover': {
-                      background: (t) => alpha(t.palette.primary.main, 0.1),
-                    },
-                  }}
-                >
-                  <Box
-                    sx={{
-                      fontSize: (t) => styleUtils(t).fontSize.small,
-                      fontWeight: (t) => t.typography.fontWeightBold,
-                      transition: (t) =>
-                        t.transitions.create('width', { duration: 1000 }),
-                      width: 0,
-                      display: 'flex',
-                      alignItems: 'center',
-                      justifyContent: 'flex-end',
-                      minHeight: 24,
-                      borderBottom: (t) =>
-                        `4px solid ${t.palette.primary.main}`,
-                      color: (t) => t.palette.primary.main,
-                    }}
+                <div style={{ flex: 1 }} className="mt-[-9px]">
+                  <div
+                    className="text-xs font-bold flex items-center justify-end min-h-[24px] text-scbluefrance border-solid border-b-scbluefrance border-b-4"
                     style={{
-                      width: appeared ? `calc(${percentOfMax * 0.9}%)` : 0,
-                      color: item.color,
-                      borderColor: item.color,
+                      width: appeared ? `calc(${percentOfMax}%)` : 0,
+                      ...(item.color
+                        ? { color: item.color, borderColor: item.color }
+                        : null),
                     }}
                   >
                     {percentOfMax > 5 && (
-                      <div>{formatLargeNumber(item.value)}</div>
+                      <div className="bg-white">
+                        {formatLargeNumber(item.value)}
+                      </div>
                     )}
-                  </Box>
-                </Box>
+                  </div>
+                </div>
               </LightTooltip>
-            </Box>
+            </div>
           )
         })
       ) : (
         <p>{m.noDataAtm} </p>
       )}
       {grid && data && data.length > 0 && (
-        <Box sx={sx.item}>
-          <Box sx={sx.label} style={{ width: width, minWidth: width }} />
-          <div style={{ position: 'relative', width: '100%' }}>
-            {mapFor(gridAxis + 1, (i) => (
-              <Box
+        <div className="flex my-2">
+          <div
+            className="self-end "
+            style={{ width: firstColumnWidth, minWidth: firstColumnWidth }}
+          />
+          <div className="relative w-full">
+            {mapFor(nbGridColumns, (i) => (
+              <div
                 key={i}
-                sx={{
-                  width: '1px',
-                  height: 1000,
-                  background: (t) => t.palette.divider,
-                  position: 'absolute',
-                  bottom: 0,
-                  right: 0,
-                  left: `calc(${i * (100 / gridAxis)}% - 1px)`,
+                className="w-[1px] h-[1000px] bg-gray-300 absolute bottom-0 right-0 z-[-10]"
+                style={{
+                  left: `calc(${i * (100 / nbGridColumns)}% - 1px)`,
                 }}
               />
             ))}
           </div>
-        </Box>
+        </div>
       )}
-    </Box>
+    </div>
   )
 }
 
