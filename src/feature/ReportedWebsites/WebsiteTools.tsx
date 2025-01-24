1c397e09bdd62022f0b829a5b643473fbf0d0fa2
Charles Dufour <charles.dufour@beta.gouv.fr>
2025-01-24 18:15:14 +0100

    [TRELLO-2775] WIP

--- src/feature/ReportedWebsites/WebsiteTools.tsx
+++ src/feature/ReportedWebsites/WebsiteTools.tsx
@@ -14,9 +14,15 @@ import internetArchiveIcon from './img/internetArchive.svg'
 import scamdocIcon from './img/scamdoc.png'
 import { useI18n } from '../../core/i18n'
 import { WebsiteWithCompany } from '../../core/client/website/Website'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { useApiContext } from '../../core/context/ApiContext'
+import { useToast } from '../../core/context/toastContext'
+import { WebsiteWithCompanySearchKeys } from '../../core/queryhooks/websiteQueryHooks'
+import { SiretExtraction } from './SiretExtraction'
 
 interface WebsiteActionsProps extends BoxProps {
   website: WebsiteWithCompany
+  onRemove: (id: string) => Promise<any>
 }
 
 const websiteAnalyzeTool = [
@@ -42,8 +48,24 @@ const websiteAnalyzeTool = [
   },
 ]
 
-export const WebsiteTools = ({ website }: WebsiteActionsProps) => {
+export const WebsiteTools = ({ website, onRemove }: WebsiteActionsProps) => {
   const { m } = useI18n()
+  const { api } = useApiContext()
+  const queryClient = useQueryClient()
+  const { toastError, toastSuccess } = useToast()
+
+  const _extractSiret = useMutation({
+    mutationFn: () => api.secured.siretExtractor.extractSiret(website.host),
+    onSuccess: () =>
+      queryClient.invalidateQueries({
+        queryKey: WebsiteWithCompanySearchKeys,
+      }),
+    onError: () =>
+      toastError({
+        message: "Une erreur est survenue dans le service d'extraction.",
+      }),
+  })
+
   return (
     <Tooltip title={m.identicationTools}>
       <ScMenu icon="travel_explore">
@@ -65,6 +87,21 @@ export const WebsiteTools = ({ website }: WebsiteActionsProps) => {
             <ListItemText>{tool.label}</ListItemText>
           </MenuItem>
         ))}
+        <MenuItem key="siret_extraction" onClick={() => _extractSiret.mutate()}>
+          <SiretExtraction
+            websiteWithCompany={website}
+            siretExtraction={_extractSiret.data}
+            isLoading={_extractSiret.isPending}
+            remove={() => onRemove(website.id)}
+            identify={() =>
+              queryClient.invalidateQueries({
+                queryKey: WebsiteWithCompanySearchKeys,
+              })
+            }
+          >
+            <div>Extraire un SIRET/SIREN</div>
+          </SiretExtraction>
+        </MenuItem>
       </ScMenu>
     </Tooltip>
   )
