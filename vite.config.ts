6f62bc0c0ff377de938f992c301f8aac9855a176
Charles Dufour <charles.dufour@beta.gouv.fr>
2025-02-10 14:16:44 +0100

    [TRELLO-2563] Add eslint & update node

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
+import eslint from 'vite-plugin-eslint'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react(), viteTsconfigPaths(), tailwindcss()],
+  // plugins: [react(), viteTsconfigPaths(), tailwindcss(), eslint()],
+  plugins: [react(), viteTsconfigPaths(), tailwindcss()], // Remove this line to enable eslint
   server: {
     // this ensures that the browser opens upon server start
     open: true,
