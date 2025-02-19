6aed17bd4ed01de6fd8b1baf5defefb45720f7ff
Said SEDOUD <s.sedoud.betagouv@gmail.com>
2025-02-19 09:48:48 +0100

    TRELLO-2850 : add annuaire des entreprise link

--- src/feature/Company/AnnuaireDesEntreprisesBanner.tsx
+++ src/feature/Company/AnnuaireDesEntreprisesBanner.tsx
@@ -1,23 +1,43 @@
-import { Alert } from 'alexlibs/mui-extension'
 import { useConnectedContext } from 'core/context/ConnectedContext'
+import React from 'react'
+import { Button, Tooltip } from '@mui/material'
+import logoGouvMobile from './annuaire-entreprises.svg'
+import { Launch } from '@mui/icons-material'
 
-export function AnnuaireDesEntreprisesBanner() {
+export function AnnuaireDesEntreprisesBanner({
+  companySiret,
+}: {
+  companySiret: string
+}) {
   const { connectedUser } = useConnectedContext()
+
   if (connectedUser.isNotPro) {
     return (
-      <Alert type="info">
-        Connectez-vous sur{' '}
-        <a
-          href={'https://annuaire-entreprises.data.gouv.fr/lp/agent-public'}
-          target="_blank"
-          rel="noreferrer"
+      <div className="justify-end mb-1 text-scbluefrance">
+        <Tooltip
+          title={
+            "Voir sur Annuaire des Entreprises, un site de l'État pour accéder aux informations protégées des entreprises (statuts, actes, bilans financiers)."
+          }
         >
-          l'Annuaire des Entreprises
-        </a>{' '}
-        pour accéder aux informations protégées des entreprises (non
-        diffusibles, statuts, actes, bilans financiers).
-      </Alert>
+          <Button
+            href={`https://annuaire-entreprises.data.gouv.fr/etablissement/${companySiret.trim()}`}
+            target="_blank"
+            rel="noreferrer"
+            variant="text"
+            color="primary"
+            endIcon={<Launch />} // Assuming 'launch' is an icon you want to display after the button text
+            onClick={(_) => _}
+          >
+            <img
+              src={logoGouvMobile} // Ensure this is an SVG or high-resolution image
+              alt="voir sur annuaire des entreprise"
+              className="h-[60px] image-rendering-auto"
+            />
+          </Button>
+        </Tooltip>
+      </div>
     )
   }
+
   return null
 }
