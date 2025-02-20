abebf507ef992bd6074513e55f49fe7928b3da1f
Said SEDOUD <s.sedoud.betagouv@gmail.com>
2025-02-20 09:29:49 +0100

    remove tooltip

--- src/feature/Company/AnnuaireDesEntreprisesBanner.tsx
+++ src/feature/Company/AnnuaireDesEntreprisesBanner.tsx
@@ -1,6 +1,6 @@
 import { useConnectedContext } from 'core/context/ConnectedContext'
 import React from 'react'
-import { Button, Tooltip } from '@mui/material'
+import { Button } from '@mui/material'
 import logoGouvMobile from './annuaire-entreprises.svg'
 import { Launch } from '@mui/icons-material'
 
@@ -14,27 +14,21 @@ export function AnnuaireDesEntreprisesBanner({
   if (connectedUser.isNotPro) {
     return (
       <div className="justify-end mb-1 text-scbluefrance">
-        <Tooltip
-          title={
-            "Voir sur Annuaire des Entreprises, un site de l'État pour accéder aux informations protégées des entreprises (statuts, actes, bilans financiers)."
-          }
+        <Button
+          href={`https://annuaire-entreprises.data.gouv.fr/etablissement/${companySiret.trim()}`}
+          target="_blank"
+          rel="noreferrer"
+          variant="text"
+          color="primary"
+          endIcon={<Launch />} // Assuming 'launch' is an icon you want to display after the button text
+          onClick={(_) => _}
         >
-          <Button
-            href={`https://annuaire-entreprises.data.gouv.fr/etablissement/${companySiret.trim()}`}
-            target="_blank"
-            rel="noreferrer"
-            variant="text"
-            color="primary"
-            endIcon={<Launch />} // Assuming 'launch' is an icon you want to display after the button text
-            onClick={(_) => _}
-          >
-            <img
-              src={logoGouvMobile} // Ensure this is an SVG or high-resolution image
-              alt="voir sur annuaire des entreprise"
-              className="h-[60px] image-rendering-auto"
-            />
-          </Button>
-        </Tooltip>
+          <img
+            src={logoGouvMobile} // Ensure this is an SVG or high-resolution image
+            alt="voir sur annuaire des entreprise"
+            className="h-[60px] image-rendering-auto"
+          />
+        </Button>
       </div>
     )
   }
