26014a86c3627eb3ad8534595e82940221d2e67b
eletallbetagouv <107104509+eletallbetagouv@users.noreply.github.com>
2025-04-15 15:26:24 +0300

    css

--- src/feature/Users/UsersProMassManage.tsx
+++ src/feature/Users/UsersProMassManage.tsx
@@ -1,7 +1,22 @@
+import PlaceOutlinedIcon from '@mui/icons-material/PlaceOutlined'
+import { Button, Checkbox, CircularProgress } from '@mui/material'
+import { Link } from '@tanstack/react-router'
+import {
+  CompanyWithAccessAndCounts,
+  flattenProCompaniesExtended,
+} from 'core/model'
+import { useGetAccessibleByProExtendedQuery } from 'core/queryhooks/companyQueryHooks'
 import { Page, PageTitle } from 'shared/Page'
 import { CleanDiscreetPanel } from 'shared/Panel/simplePanels'
 
 export function AccessesManagementPro() {
+  const _companiesAccessibleByPro = useGetAccessibleByProExtendedQuery()
+  const myCompanies = _companiesAccessibleByPro.data
+  const allCompaniesIds =
+    (myCompanies &&
+      flattenProCompaniesExtended(myCompanies).map((_) => _.company.id)) ??
+    []
+
   return (
     <Page>
       <PageTitle>Gestion des accès</PageTitle>
@@ -20,10 +35,118 @@ export function AccessesManagementPro() {
         <h2 className="font-bold text-2xl">
           Sélectionner une ou plusieurs entreprises
         </h2>
+        <div className="">
+          {myCompanies ? (
+            <div className="">
+              {myCompanies.headOfficesAndSubsidiaries.map(
+                ({ headOffice, subsidiaries }) => {
+                  return (
+                    <TopLevelRow
+                      key={headOffice.company.id}
+                      company={headOffice}
+                      secondLevel={subsidiaries}
+                    />
+                  )
+                },
+              )}
+              {myCompanies.loneSubsidiaries.map((company) => {
+                return <TopLevelRow key={company.company.id} {...{ company }} />
+              })}
+            </div>
+          ) : (
+            <CircularProgress />
+          )}
+        </div>
         <h2 className="font-bold text-2xl">
           Sélectionner un ou plusieurs utilisateurs
         </h2>
       </div>
     </Page>
+  )
+}
+
+function TopLevelRow({
+  company,
+  secondLevel,
+}: {
+  company: CompanyWithAccessAndCounts
+  secondLevel?: CompanyWithAccessAndCounts[]
+}) {
+  return (
+    <div className="">
+      <RowContent {...{ company }} isTopLevel={true} />
+      {secondLevel ? <SecondLevelWrapper {...{ secondLevel }} /> : null}
+    </div>
+  )
+}
+
+function SecondLevelRow({ company }: { company: CompanyWithAccessAndCounts }) {
+  return <RowContent {...{ company }} isTopLevel={false} />
+}
+
+function RowContent({
+  company: _company,
+  isTopLevel,
+}: {
+  company: CompanyWithAccessAndCounts
+  isTopLevel: boolean
+}) {
+  const { company } = _company
+  const companyId = company.id
+
+  return (
+    <div
+      className={`border border-solid border-black bg-white  ${isTopLevel ? 'p-1 py-3' : 'p-1 py-2'}`}
+    >
+      <div className="flex gap-2 justify-between">
+        <div className="flex gap-2">
+          <div className="">
+            <Checkbox className="!p-0" />
+          </div>
+          <p className="w-34">{company.siret}</p>
+          <div className="">
+            <Link
+              to="/entreprise/$companyId/bilan"
+              params={{ companyId }}
+              className={`text-scbluefrance`}
+            >
+              {company.name}
+            </Link>
+            {company.isHeadOffice ? ' (siège social)' : null}
+          </div>
+        </div>
+        <div className="">
+          <PlaceOutlinedIcon className="!text-[1.1em] -mt-0.5" />
+
+          {company.address.postalCode}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+function SecondLevelWrapper({
+  secondLevel,
+}: {
+  secondLevel: CompanyWithAccessAndCounts[]
+}) {
+  if (secondLevel.length === 0) {
+    return null
+  }
+  return (
+    <div className="ml-15 mt-2 mb-4">
+      <div className="flex gap-2 items-center mb-0">
+        <span className="">
+          {secondLevel.length} établissements secondaires
+        </span>
+        <Button size="small">Sélectionner tous</Button>
+        <Button size="small">Désélectionner tous</Button>
+      </div>
+      <div className="">
+        {secondLevel.map((c) => {
+          return <SecondLevelRow key={c.company.id} {...{ company: c }} />
+        })}
+      </div>
+    </div>
   )
 }
