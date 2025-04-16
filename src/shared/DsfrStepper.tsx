d332bacba01250e8ef361f4e2f77ebe99f9be246
eletallbetagouv <107104509+eletallbetagouv@users.noreply.github.com>
2025-04-16 15:38:45 +0300

    improve stepper

--- src/shared/DsfrStepper.tsx
+++ src/shared/DsfrStepper.tsx
@@ -1,22 +1,26 @@
+import { Button, Icon } from '@mui/material'
+
 // roughly mimics https://www.systeme-de-design.gouv.fr/composants-et-modeles/composants/indicateur-d-etapes/
 export function DsfrStepper({
   currentStep,
   steps,
+  onPrevious,
 }: {
   currentStep: number
   steps: string[]
+  onPrevious?: () => void
 }) {
   const step = steps[currentStep]
   const isLastStep = currentStep === steps.length - 1
   const nextStep = isLastStep ? null : steps[currentStep + 1]
   return (
-    <div className="mb-20 max-w-[500px]">
+    <div className="mb-20">
       <span className="text-gray-500 mb-5 text-sm">
         Étape {currentStep + 1} sur {steps.length}
       </span>
       <h2 className="font-bold text-xl mb-3">{step}</h2>
-      <div className="flex gap-2 w-full mb-4">
-        {steps.map((s, idx) => {
+      <div className="flex gap-2 w-full">
+        {steps.map((_, idx) => {
           return (
             <div
               key={idx}
@@ -25,11 +29,20 @@ export function DsfrStepper({
           )
         })}
       </div>
-      {nextStep && (
-        <p className="text-gray-500 text-xs">
-          <span className="font-bold">Étape suivante :</span> {nextStep}
-        </p>
-      )}
+      <div className="flex justify-between items-center">
+        <Button
+          startIcon={<Icon>keyboard_arrow_left</Icon>}
+          className="!capitalize"
+          onClick={onPrevious}
+        >
+          Précédent
+        </Button>
+        {nextStep && (
+          <p className="text-gray-500 text-sm">
+            <span className="font-bold">Étape suivante :</span> {nextStep}
+          </p>
+        )}
+      </div>
     </div>
   )
 }
