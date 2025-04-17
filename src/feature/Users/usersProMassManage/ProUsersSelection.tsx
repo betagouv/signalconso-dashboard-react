317683bf92946703001de4b1d8ae595303e7014c
eletallbetagouv <107104509+eletallbetagouv@users.noreply.github.com>
2025-04-17 15:40:22 +0300

    suite

--- src/feature/Users/usersProMassManage/ProUsersSelection.tsx
+++ src/feature/Users/usersProMassManage/ProUsersSelection.tsx
@@ -1,7 +1,8 @@
-import { Checkbox } from '@mui/material'
+import { Button, Checkbox } from '@mui/material'
 import { useConnectedContext } from 'core/context/connected/connectedContext'
 import { User } from 'core/model'
 import { useUsersOfProQuery } from 'core/queryhooks/accessesMassManagementQueryHooks'
+import { SetStateAction, useState } from 'react'
 import { Controller, useForm, UseFormReturn } from 'react-hook-form'
 import { CleanInvisiblePanel } from 'shared/Panel/simplePanels'
 import { TinyButton } from './usersProMassManageTinyComponents'
@@ -10,6 +11,16 @@ type FormShape = {
   selection: { [id: string]: boolean }
 }
 type Form = UseFormReturn<FormShape>
+
+type RowData =
+  | {
+      kind: 'to_invite'
+      email: string
+    }
+  | {
+      kind: 'actual_user'
+      user: User
+    }
 
 export function ProUsersSelection() {
   const _query = useUsersOfProQuery()
@@ -23,6 +34,10 @@ export function ProUsersSelection() {
 
 function Loaded({ data }: { data: User[] }) {
   const { connectedUser } = useConnectedContext()
+  const [usersToInvite, setUsersToInvite] = useState<string[]>([
+    'george@gmail.com',
+    'walalla@gmail.com',
+  ])
   const allIds = data.map((_) => _.id)
   const form = useForm<FormShape>({
     defaultValues: {
@@ -57,46 +72,102 @@ function Loaded({ data }: { data: User[] }) {
           </div>
           <TinyButton label="Inviter" onClick={() => {}} />
         </div>
+        {usersToInvite.map((email) => {
+          return (
+            <RowContent
+              key={email}
+              rowData={{ kind: 'to_invite', email }}
+              {...{ form, setUsersToInvite }}
+            />
+          )
+        })}
         {data.map((user) => {
-          return <RowContent key={user.id} user={user} form={form} />
+          return (
+            <RowContent
+              key={user.id}
+              rowData={{ kind: 'actual_user', user }}
+              {...{ form, setUsersToInvite }}
+            />
+          )
         })}
       </div>
     </>
   )
 }
 
-function RowContent({ user, form }: { user: User; form: Form }) {
+function RowContent({
+  rowData,
+  form,
+  setUsersToInvite,
+}: {
+  rowData: RowData
+  form: Form
+  setUsersToInvite: React.Dispatch<SetStateAction<string[]>>
+}) {
   const { connectedUser } = useConnectedContext()
-  const disabled = shouldBeDisabled(user, connectedUser)
+  const disabled =
+    rowData.kind === 'actual_user'
+      ? shouldBeDisabled(rowData.user, connectedUser)
+      : false
   return (
     <div
       className={`border-t ${disabled ? 'bg-gray-100 border-gray-300 text-gray-500 border-x' : 'bg-white border-gray-400'} last:border-b-1 px-2 py-3`}
     >
       <div className="flex gap-2 items-center">
-        <div className={`mx-6 h-fit`}>
-          <Controller
-            control={form.control}
-            name={`selection.${user.id}`}
-            render={({ field: { onChange, onBlur, value, ref } }) => {
-              return (
-                <Checkbox
-                  className="!p-0 "
-                  disabled={disabled}
-                  checked={value}
-                  {...{ onBlur, onChange }}
-                  slotProps={{ input: { ref } }}
-                />
-              )
-            }}
-          />
-        </div>
-        <div className="flex flex-col gap-1">
-          <p>{user.email}</p>
-          <div className="">
-            {user.firstName} {user.lastName}
-          </div>
-        </div>
-        {disabled && <p className="text-sm text-right grow"> c'est vous !</p>}
+        {rowData.kind === 'actual_user' ? (
+          <>
+            <div className={`mx-6 h-fit`}>
+              <Controller
+                control={form.control}
+                name={`selection.${rowData.user.id}`}
+                render={({ field: { onChange, onBlur, value, ref } }) => {
+                  return (
+                    <Checkbox
+                      className="!p-0 "
+                      disabled={disabled}
+                      checked={value}
+                      {...{ onBlur, onChange }}
+                      slotProps={{ input: { ref } }}
+                    />
+                  )
+                }}
+              />
+            </div>
+            <div className="flex flex-col gap-0 ">
+              <p>{rowData.user.email}</p>
+              <div className="text-sm">
+                {rowData.user.firstName} {rowData.user.lastName}
+              </div>
+            </div>
+            {disabled && (
+              <p className="text-sm text-right grow"> c'est vous !</p>
+            )}
+          </>
+        ) : (
+          <>
+            <div className={`mx-6 h-fit`}>
+              <Checkbox className="!p-0 " disabled checked />
+            </div>
+            <div>
+              <p>{rowData.email}</p>
+              <p className="text-sm italic">
+                Une invitation sera envoyée{' '}
+                <Button
+                  variant="text"
+                  onClick={() => {
+                    setUsersToInvite((prev) =>
+                      prev.filter((_) => _ !== rowData.email),
+                    )
+                  }}
+                  size="small"
+                  className="!py-0 !-mt-0.5"
+                >
+                  annuler
+                </Button>
+              </p>
+            </div>
+          </>
+        )}
       </div>
     </div>
   )
