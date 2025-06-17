138f2347ebb08bbd1bbd106345599291761ee3c7
Charles Dufour <charles.dufour@beta.gouv.fr>
2025-06-17 10:43:46 +0200

    Revert index.html

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -17,34 +17,6 @@ interface RouterContext {
 }
 
 export const Route = createRootRouteWithContext<RouterContext>()({
-  head: () => ({
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { name: 'theme-color', content: '#000000' },
-      {
-        name: 'description',
-        content:
-          "L'espace Pro vous permet de gérer et consulter les signalements dont vous faites l'objet",
-      },
-      { name: 'robots', content: 'noindex' },
-    ],
-    links: [
-      { rel: 'icon', href: '/favicon.ico' },
-      { rel: 'apple-touch-icon', href: '/logo192.png' },
-      { rel: 'manifest', href: '/manifest.json' },
-      { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
-      {
-        rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500&display=swap',
-      },
-      {
-        rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/icon?family=Material+Icons',
-      },
-      { rel: 'stylesheet', href: '/fonts.css' },
-    ],
-  }),
   component: RouteComponent,
 })
 
